ad80eb78f599c418e230d080933809121b77623d
SkirnevskyiAleksandr <sanya-sha2016@ukr.net>
2022-02-04 18:02:05 +0200

    feat: changing activ slide by function

--- app.js
+++ app.js
@@ -1,14 +1,18 @@
-const slides = document.querySelectorAll('.slide')
-const slideWrapper = document.querySelector('.container')
+function slidesPlugin(activeSlide = 1) {
+    const slides = document.querySelectorAll('.slide')
+    const slideWrapper = document.querySelector('.container')
+    slides[activeSlide].classList.add('active')
 
-for (const item of slides) {
-    item.addEventListener('click', () => {
-        removeActive()
-        item.classList.add('active')
-    })
+    for (const item of slides) {
+        item.addEventListener('click', () => {
+            removeActive()
+            item.classList.add('active')
+        })
+    }
+    function removeActive() {
+        slides.forEach((item) => {
+            item.classList.remove('active')
+        })
+    }
 }
-function removeActive() {
-    slides.forEach((item) => {
-        item.classList.remove('active')
-    })
-}
+slidesPlugin()
